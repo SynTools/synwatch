07e6986f9e74bf0bdc18621eef9f2e6061e84988
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2025-10-16 14:46:50 +0200

    feat: render project overview

--- lib/synwatch_web/controllers/project_controller.ex
+++ lib/synwatch_web/controllers/project_controller.ex
@@ -7,8 +7,6 @@ defmodule SynwatchWeb.ProjectController do
   def index(%{assigns: %{current_user: %User{} = user}} = conn, _params) do
     projects = Projects.get_all_by_user_id(user.id)
 
-    # TODO: Render projects in template
-    IO.inspect(projects)
-    render(conn, :index, page_title: "Projects")
+    render(conn, :index, page_title: "Projects", projects: projects)
   end
 end
--- lib/synwatch_web/controllers/project_html.ex
+++ lib/synwatch_web/controllers/project_html.ex
@@ -2,4 +2,27 @@ defmodule SynwatchWeb.ProjectHTML do
   use SynwatchWeb, :html
 
   embed_templates "project_html/*"
+
+  defp format_dt(nil), do: "-"
+
+  defp format_dt(%NaiveDateTime{} = ndt),
+    do: Calendar.strftime(ndt, "%Y-%m-%d · %H:%M")
+
+  defp format_dt(%DateTime{} = dt),
+    do: dt |> DateTime.to_naive() |> format_dt()
+
+  defp format_relative(nil), do: ""
+  defp format_relative(%DateTime{} = dt), do: format_relative(DateTime.to_naive(dt))
+
+  defp format_relative(%NaiveDateTime{} = ndt) do
+    diff = NaiveDateTime.diff(NaiveDateTime.utc_now(), ndt, :second)
+
+    cond do
+      diff < 60 -> "just now"
+      diff < 3600 -> "#{div(diff, 60)} min ago"
+      diff < 86_400 -> "#{div(diff, 3600)} h ago"
+      diff < 2 * 86_400 -> "yesterday"
+      true -> "#{div(diff, 86_400)} d ago"
+    end
+  end
 end
