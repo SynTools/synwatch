d2aa47585b1d3364720e563eab064d6e5abe7353
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2025-11-22 20:49:01 +0100

    feat: get and render user's teams

---
+++ lib/synwatch/teams.ex
@@ -0,0 +1,16 @@
+defmodule Synwatch.Teams do
+  import Ecto.Query, warn: false
+
+  alias Synwatch.Repo
+  alias Synwatch.Accounts.Team
+
+  def get_all_for_user(user_id) do
+    from(t in Team,
+      join: tm in "team_memberships",
+      on: tm.team_id == t.id,
+      where: tm.user_id == type(^user_id, :binary_id),
+      preload: [:members, :owner]
+    )
+    |> Repo.all()
+  end
+end
--- lib/synwatch_web/controllers/settings_controller.ex
+++ lib/synwatch_web/controllers/settings_controller.ex
@@ -1,7 +1,14 @@
 defmodule SynwatchWeb.SettingsController do
   use SynwatchWeb, :controller
 
-  def index(conn, _params) do
-    render(conn, :index, page_title: "Settings")
+  alias Synwatch.Accounts.User
+  alias Synwatch.Teams
+
+  def index(%Plug.Conn{assigns: %{current_user: %User{} = user}} = conn, _params) do
+    teams = Teams.get_all_for_user(user.id)
+
+    IO.inspect(teams)
+
+    render(conn, :index, page_title: "Settings", teams: teams)
   end
 end
